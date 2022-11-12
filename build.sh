48674d1e71763a2e1ddfffa60b56d3c61bbd549c
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-12 19:38:39 -0300

    :wrench: chore adds automatic repositories cloning

---
+++ build.sh
@@ -0,0 +1,3 @@
+git clone -b dev git@github.com:aloefflerj/astro-focus-kit-server.git ../astro-focus-kit-server
+git clone -b dev git@github.com:aloefflerj/astro-focus-kit-client.git ../astro-focus-kit-client
+git clone git@github.com:aloefflerj/astro-focus-kit-chrome-extension.git ../astro-focus-kit-chrome-extension
