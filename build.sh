b525cdf09d46417fc3eeb6cf8c7139c8f213af32
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-18 21:26:49 -0300

    :bricks: build: adds script to build application

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 git clone -b dev git@github.com:aloefflerj/astro-focus-kit-server.git ../astro-focus-kit-server
 git clone -b dev git@github.com:aloefflerj/astro-focus-kit-client.git ../astro-focus-kit-client
 git clone git@github.com:aloefflerj/astro-focus-kit-chrome-extension.git ../astro-focus-kit-chrome-extension
+
+cp ../astro-focus-kit-client/example.env .env
+cp ../astro-focus-kit-chrome-extension/.env.example .env
+cp example.env .env
+
+docker-compose build
+docker-compose up -d
